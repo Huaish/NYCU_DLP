10240af8d8ffbb04d9a294eaa7fc83d96de209bd
Huaish <zsisc.huaish@gmail.com>
2024-07-27 03:46:26 +0800

    fix(lab3): fix wrong dice score function

--- lab3/src/evaluate.py
+++ lab3/src/evaluate.py
@@ -1,18 +1,19 @@
 import torch
-from utils import dice_score
+import numpy as np
+from utils import dice_score, dice_score_loss
 
 def evaluate(net, data, device):
     net.eval()
     net.to(device)
-    dice = 0.0
+    dice_scores = []
+    loss = []
+    
     with torch.no_grad():
         for i, batch in enumerate(data):
-            images, masks = batch
-            images = images.to(device)
-            masks = masks.to(device)
-            outputs = net(images)
-            for j in range(len(outputs)):
-                pred_mask = outputs[j].cpu().numpy()
-                gt_mask = masks[j].cpu().numpy()
-                dice += dice_score(pred_mask, gt_mask)
-    return dice / len(data)
+            images = batch["image"].to(device)
+            masks = batch["mask"].to(device)
+            output = net(images)
+            dice_scores.append(dice_score(output, masks))
+            loss.append(dice_score_loss(output, masks))
+            
+    return np.mean(dice_scores), np.mean(loss)
--- lab3/src/utils.py
+++ lab3/src/utils.py
@@ -1,19 +1,44 @@
 import numpy as np
+import torch
+import termcolor
 
-def dice_score(pred_mask, gt_mask):
+def dice_score(pred_mask, gt_mask, epsilon=1e-6):
     """
     Computes the Dice similarity coefficient between two binary masks.
+    
+    Args:
+        pred_mask (Tensor): predicted  torch.Size([16, 2, 256, 256])
+        gt_mask (Tensor): ground truth torch.Size([16, 256, 256])
+        
+    Returns:
+        float: Dice similarity coefficient.
+    """
+    # Convert predicted mask to class labels
+    pred_mask = torch.argmax(pred_mask, dim=1) # torch.Size([16, 256, 256])
+    
+    # Flatten the tensors
+    pred_mask = pred_mask.view(-1).float()
+    gt_mask = gt_mask.view(-1).float()
+    
+    # Calculate intersection and union
+    intersection = (pred_mask * gt_mask).sum()
+    union = pred_mask.sum() + gt_mask.sum()
+    
+    # Compute Dice coefficient
+    dice = (2. * intersection + epsilon) / (union + epsilon)
+    
+    return dice.item()
+
+
+def dice_score_loss(pred_mask, gt_mask):
+    """
+    Computes the Dice loss between two binary masks.
     
     Args:
         pred_mask (np.array): predicted binary mask of shape (H, W).
         gt_mask (np.array): ground truth binary mask of shape (H, W).
         
     Returns:
-        float: Dice similarity coefficient.
+        float: Dice loss.
     """
-    pred_mask = pred_mask.astype(np.bool)
-    gt_mask = gt_mask.astype(np.bool)
-    
-    intersection = np.logical_and(pred_mask, gt_mask)
-    return 2. * intersection.sum() / (pred_mask.sum() + gt_mask.sum())
-
+    return 1 - dice_score(pred_mask, gt_mask)
