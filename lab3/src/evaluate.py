c599728ff6af5184d417adf7c70ccf6049e50092
Huaish <zsisc.huaish@gmail.com>
2024-07-28 02:39:45 +0800

    feat(lab3): add progress bar when evaluating

--- lab3/src/evaluate.py
+++ lab3/src/evaluate.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from tqdm import tqdm
 from utils import dice_score, dice_score_loss
 
 def evaluate(net, data, device):
@@ -9,11 +10,14 @@ def evaluate(net, data, device):
     loss = []
     
     with torch.no_grad():
-        for i, batch in enumerate(data):
-            images = batch["image"].to(device)
-            masks = batch["mask"].to(device)
-            output = net(images)
-            dice_scores.append(dice_score(output, masks))
-            loss.append(dice_score_loss(output, masks))
+        with tqdm(total=len(data), desc="Evaluation", unit="batch", leave=False) as pbar:
+            for i, batch in enumerate(data):
+                images = batch["image"].to(device)
+                masks = batch["mask"].to(device)
+                output = net(images)
+                dice_scores.append(dice_score(output, masks))
+                loss.append(dice_score_loss(output, masks))
+                
+                pbar.update()
             
     return np.mean(dice_scores), np.mean(loss)
