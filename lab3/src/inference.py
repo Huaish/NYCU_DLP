691e14fa44cbec70af95ff31477db78f6c6403b6
Huaish <zsisc.huaish@gmail.com>
2024-07-28 02:42:01 +0800

    fix(lab3): inference using gpu

--- lab3/src/inference.py
+++ lab3/src/inference.py
@@ -10,14 +10,14 @@ def test(args):
     data_path = args.data_path
     batch_size = args.batch_size
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cpu")
     print(f"Using device {device}")
     
     # Load the data
     print(f"Loading data from {data_path}")
     test_dataset = load_dataset(data_path, "test")
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    
+    print(f"Loaded {len(test_dataset)} test images")
+
     # Load the model
     model = UNet(in_channels=3, out_channels=2).to(device)
     model.load_state_dict(torch.load(args.model))
@@ -34,7 +34,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
     parser.add_argument('--model', default='../saved_models/latest.pth', help='path to the stored model weoght')
     parser.add_argument('--data_path', type=str, default="../dataset/oxford-iiit-pet", help='path to the input data')
-    parser.add_argument('--batch_size', '-b', type=int, default=1, help='batch size')
+    parser.add_argument('--batch_size', '-b', type=int, default=20, help='batch size')
     
     return parser.parse_args()
 
