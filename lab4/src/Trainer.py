0637e176488d4161f3746d1ed6bb21580cda8ef5
Huaish <zsisc.huaish@gmail.com>
2024-08-08 04:37:11 +0800

    fix(lab4): speed up

--- lab4/src/Trainer.py
+++ lab4/src/Trainer.py
@@ -39,8 +39,6 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
 
 class kl_annealing():
