a54bd6c5647228afc3aaf40da3fab11dbd080782
Huaish <zsisc.huaish@gmail.com>
2024-08-19 19:03:29 +0800

    feat(lab6): implement ddpm

---
+++ lab6/src/models/ddpm.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+from diffusers import UNet2DModel
+
+class ConditionalDDPM(nn.Module):
+    def __init__(self, num_labels=24, dim=256):
+        super().__init__()
+        self.label_embedding = nn.Linear(num_labels, dim)
+        self.diffusion = UNet2DModel(
+            sample_size=64,
+            in_channels=3,
+            out_channels=3,
+            layers_per_block=2,
+            block_out_channels=(dim//4, dim//4, dim//2, dim//2, dim, dim),
+            down_block_types=[
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "AttnDownBlock2D",
+                "AttnDownBlock2D",
+                "AttnDownBlock2D",
+            ],
+            up_block_types=[
+                "AttnUpBlock2D",
+                "AttnUpBlock2D",
+                "AttnUpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+            ],
+            class_embed_type="identity",
+            num_class_embeds=dim
+        )
+        
+        
+    def forward(self, x, timesteps, label):
+        label = self.label_embedding(label)
+        return self.diffusion(x, timesteps, label).sample
