fcdb8f0ed03e9f76d9eab999927a8fa54364706f
Huaish <zsisc.huaish@gmail.com>
2024-08-22 00:01:46 +0800

    chore(lab6): modify comment and code for testing

--- lab6/src/dataloader.py
+++ lab6/src/dataloader.py
@@ -53,7 +53,7 @@ class LoadTestData(torchData):
     
     Args:
         root: Dataset Path
-        test_json: JSON file containing image paths
+        test_json: JSON file containing labels
         object_json: JSON file containing object labels
     """
     def __init__(self, root, test_json, object_json):
@@ -98,36 +98,4 @@ if __name__ == '__main__':
     print(test_dataset.info)
     label = test_dataset[0]
     print(label)
-    
-    # Test the show_images function
-    print("Testing show_images function")
-    from utils import show_images
-    images = []
-    for i in range(10):
-        images.append(train_dataset[i][0])
-        
-    transforms = transforms.Compose([
-        transforms.Normalize(mean=[-1, -1, -1], std=[2, 2, 2])
-    ])
-        
-    show_images(images)
-    
-    print("Testing noise scheduler")
-    from diffusers import DDPMScheduler
-    import matplotlib.pyplot as plt
-    noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
-    
-    plt.clf()
-    plt.plot(noise_scheduler.alphas_cumprod.cpu() ** 0.5, label='Noise Level')
-    plt.plot((1 - noise_scheduler.alphas_cumprod.cpu()) ** 0.5, label='Signal Level')
-    plt.xlabel('Timesteps')
-    plt.ylabel('Noise Level')
-    plt.legend()
-    plt.savefig('noise_level.png')
-
-    timesteps = torch.linspace(0, 200, 10).long()
-    clean_image =torch.stack([train_dataset[0][0]] * 10)
-    noise = torch.randn_like(clean_image)
-    noisy_image = noise_scheduler.add_noise(clean_image, noise, timesteps)
-    show_images(noisy_image, title='Denoising Process (Noisy to Clear)', save_path="denoising_process.png", denoising_process=True)
         
